26458b224f22045a4284a2709c286eff77787b06
Chandan Kumar <22887527+cksharma11@users.noreply.github.com>
2019-03-21 14:40:56 +0530

    Rename tests

--- test/com/bootcamp/unit/QuantityTest.java
+++ test/com/bootcamp/unit/QuantityTest.java
@@ -8,37 +8,51 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class QuantityTest {
     @Test
-    void shouldReturnTrueWhenOneFeetIsComparedWithTwelveInches() {
+    void assertOneFeetEqualsTwelveInches() {
         Quantity oneFeet = new Quantity(new BigDecimal(1), Unit.FEET);
         Quantity twelveInch = new Quantity(new BigDecimal(12), Unit.INCH);
         assertEquals(oneFeet, twelveInch);
     }
 
     @Test
-    void shouldReturnFalseWhenOneFeetIsComparedWithOneInch() {
+    void assertOneFeetNotEqualsOneInches() {
         Quantity oneFeet = new Quantity(new BigDecimal(1), Unit.FEET);
         Quantity twelveInch = new Quantity(new BigDecimal(1), Unit.INCH);
         assertNotEquals(oneFeet, twelveInch);
     }
 
     @Test
-    void shouldReturnTrueWhenOneMillimeterIsComparedWithOneInch() {
+    void assertOneMMEqualsTwentyFiveInches() {
         Quantity oneMillimeter = new Quantity(new BigDecimal(1), Unit.MILLIMETER);
         Quantity twelveInch = new Quantity(new BigDecimal(25), Unit.INCH);
         assertNotEquals(oneMillimeter, twelveInch);
     }
 
     @Test
-    void shouldReturnTrueWhenTwoInchesAreComparedWithFiveCentimeters() {
+    void assertTwoInchesEqualsFiveCentimeters() {
         Quantity twoInches = new Quantity(new BigDecimal(2), Unit.INCH);
         Quantity fiveCentimeters = new Quantity(new BigDecimal(5), Unit.CENTIMETER);
         assertEquals(twoInches, fiveCentimeters);
     }
 
     @Test
-    void shouldReturnFalseWhenTwoInchesAreComparedWithTwoCentimeters() {
+    void assertTwoInchesNotEqualsTwoCM() {
         Quantity twoInches = new Quantity(new BigDecimal(2), Unit.INCH);
         Quantity twoCentimeters = new Quantity(new BigDecimal(2), Unit.CENTIMETER);
         assertNotEquals(twoInches, twoCentimeters);
+    }
+
+    @Test
+    void assertOneCMEqualsTenMM() {
+        Quantity oneCM = new Quantity(new BigDecimal(1), Unit.CENTIMETER);
+        Quantity tenMM = new Quantity(new BigDecimal(10), Unit.MILLIMETER);
+        assertEquals(oneCM, tenMM);
+    }
+
+    @Test
+    void assertTwoCMNotEqualsTenMM() {
+        Quantity twoCM = new Quantity(new BigDecimal(2), Unit.CENTIMETER);
+        Quantity tenMM = new Quantity(new BigDecimal(10), Unit.MILLIMETER);
+        assertNotEquals(twoCM, tenMM);
     }
 }
