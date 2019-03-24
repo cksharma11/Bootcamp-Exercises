73e85a06e1396d1e04228e21740eec68020ca725
Chandan Kumar <22887527+cksharma11@users.noreply.github.com>
2019-03-24 19:17:49 +0530

    Extracted functions

--- src/com/bootcamp/matrix/Matrix.java
+++ src/com/bootcamp/matrix/Matrix.java
@@ -1,7 +1,6 @@
 package com.bootcamp.matrix;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
 
@@ -27,18 +26,26 @@ class Matrix {
         return this.performOperation(otherMatrix, subtractElements);
     }
 
+
     private Matrix performOperation(Matrix otherMatrix, MatrixOperation operation) {
         List<List<Integer>> list = new ArrayList<>();
         Matrix resultedMatrix = new Matrix(list);
         for (int row = 0; row < this.matrix.size(); row++) {
-            List<Integer> resultRow = new ArrayList<>();
-            for (int column = 0; column < this.matrix.size(); column++) {
-                Integer resultedElement = operation.perform(this.matrix.get(row).get(column), otherMatrix.matrix.get(row).get(column));
-                resultRow.add(resultedElement);
-            }
-            resultedMatrix.matrix.addAll(Arrays.asList(resultRow));
+            List<Integer> thisMatrixRow = this.matrix.get(row);
+            List<Integer> otherMatrixRow = otherMatrix.matrix.get(row);
+            List<Integer> operationResult = doOperation(thisMatrixRow, otherMatrixRow, operation);
+            resultedMatrix.matrix.add(operationResult);
         }
         return resultedMatrix;
+    }
+
+    private List<Integer> doOperation(List<Integer> thisRow, List<Integer> otherRow, MatrixOperation operation) {
+        List<Integer> result = new ArrayList<>();
+        for (int row = 0; row < this.matrix.size(); row++) {
+            Integer resultedElement = operation.perform(thisRow.get(row), otherRow.get(row));
+            result.add(resultedElement);
+        }
+        return result;
     }
 
     Matrix multiply(Matrix otherMatrix) {
@@ -46,30 +53,44 @@ class Matrix {
         Matrix resultedMatrix = new Matrix(list);
 
         for (int index = 0; index < this.matrix.size(); index++) {
-            List<Integer> resultRow = new ArrayList<>();
-            for (int row = 0; row < this.matrix.size(); row++) {
-                int sumOfMultiplication = 0;
-                for (int column = 0; column < otherMatrix.matrix.size(); column++) {
-                    sumOfMultiplication += (this.matrix.get(index).get(column) * otherMatrix.matrix.get(column).get(row));
-                }
-                resultRow.add(sumOfMultiplication);
-            }
+            List<Integer> resultRow = getMultipliedRows(otherMatrix, index);
             resultedMatrix.matrix.add(resultRow);
         }
         return resultedMatrix;
+    }
+
+    private List<Integer> getMultipliedRows(Matrix otherMatrix, int index) {
+        List<Integer> resultRow = new ArrayList<>();
+        for (int row = 0; row < this.matrix.size(); row++) {
+            int sumOfMultiplication = multiplyAndAdd(otherMatrix, index, row);
+            resultRow.add(sumOfMultiplication);
+        }
+        return resultRow;
+    }
+
+    private int multiplyAndAdd(Matrix otherMatrix, int index, int row) {
+        int sumOfMultiplication = 0;
+        for (int column = 0; column < otherMatrix.matrix.size(); column++) {
+            sumOfMultiplication += (this.matrix.get(index).get(column) * otherMatrix.matrix.get(column).get(row));
+        }
+        return sumOfMultiplication;
     }
 
     Matrix transpose() {
         List<List<Integer>> list = new ArrayList<>();
         Matrix resultedMatrix = new Matrix(list);
         for (int row = 0; row < this.matrix.get(0).size(); row++) {
-            List<Integer> resultRow = new ArrayList<>();
-            for (List<Integer> rowElements : this.matrix) {
-                resultRow.add(rowElements.get(row));
-            }
-            resultedMatrix.matrix.add(resultRow);
+            resultedMatrix.matrix.add(getTransposedRow(row));
         }
         return resultedMatrix;
+    }
+
+    private List<Integer> getTransposedRow(int row) {
+        List<Integer> resultRow = new ArrayList<>();
+        for (List<Integer> rowElements : this.matrix) {
+            resultRow.add(rowElements.get(row));
+        }
+        return resultRow;
     }
 
     @Override
